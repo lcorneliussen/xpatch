e0b955572f18e16a07d02f44ca85f5592106693a
Lars Corneliussen <me@lcorneliussen.de>
2010-07-20 20:04:03 +0200

    Na zumindest funktioniert es jetzt!

--- XPatch.Console/Program.cs
+++ XPatch.Console/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml;
 
 namespace XPatch.Console
 {
@@ -9,6 +10,12 @@ namespace XPatch.Console
     {
         static void Main(string[] args)
         {
+            var xml = new XmlDocument();
+            xml.Load(@"..\..\test.xml");
+            System.Console.WriteLine(xml.OuterXml);
+            xml.SelectSingleNode("xml/@attribut").InnerText = "wert2";
+            System.Console.WriteLine(xml.OuterXml);
+            System.Console.ReadLine();
         }
     }
 }
