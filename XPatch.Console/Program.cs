c3d138a23b1f8b5ab2fc0acfaa024a9f7ef35d04
Lars Corneliussen <me@lcorneliussen.de>
2010-07-20 20:44:40 +0200

    Jetzt mal inklusive Abfrage eines Wertes, wenn keiner übergeben wurde.

--- XPatch.Console/Program.cs
+++ XPatch.Console/Program.cs
@@ -11,12 +11,21 @@ namespace XPatch.Console
     {
         static void Main(string[] args)
         {
-            args = new string[] { @"..\..\test.xml", "xml/@attribut", "wert2"};
+            args = new string[] { @"..\..\test.xml", "xml/@attribut" /*, "wert2"*/};
             
             var xml = new XmlDocument();
             xml.Load(args[0]);
             System.Console.WriteLine(xml.OuterXml);
-            xml.SelectSingleNode(args[1]).InnerText = args[2];
+
+            var newValue = args.Length > 2 ? args[2] : null;
+
+            if (newValue == null)
+            {
+                System.Console.Write("Please enter the new value for '{0}':", args[1]);
+                newValue = System.Console.ReadLine();
+            }
+
+            xml.SelectSingleNode(args[1]).InnerText = newValue;
             System.Console.WriteLine(xml.OuterXml);
 
             System.Console.ReadLine();
