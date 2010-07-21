8c6fc01adf799249cdf4aa3b7e6cffaf8a0174ae
Lars Corneliussen <me@lcorneliussen.de>
2010-07-21 22:29:01 +0200

    XPatch 0.1 - Funktioniert, ist aber noch weit von benutzbar entfernt

--- XPatch.Console/Program.cs
+++ XPatch.Console/Program.cs
@@ -1,28 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Text;
+﻿using System.Diagnostics;
 using System.Xml;
+using C = System.Console;
 
 namespace XPatch.Console
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            // args = new string[] { @"..\..\test.xml", "xml/@attribut" /*, "wert2"*/};
-            
+#if (DEBUG)
+            if (Debugger.IsAttached)
+                args = new string[]
+                           {
+                               @"..\..\test.xml",
+                               "xml/@attribut",
+                               "wert2"
+                           };
+#endif
+
             var xml = new XmlDocument();
             xml.Load(args[0]);
-            // System.Console.WriteLine(xml.OuterXml);
 
             var newValue = args.Length > 2 ? args[2] : null;
 
             if (newValue == null)
             {
-                System.Console.Write("Please enter the new value for '{0}':", args[1]);
+                C.Write("Please enter the new value for '{0}': ", args[1]);
                 newValue = System.Console.ReadLine();
             }
 
@@ -31,11 +34,13 @@ namespace XPatch.Console
             element.InnerText = newValue;
 
             xml.Save(args[0]);
-            System.Console.Write("Value for '{0}' changed from '{1}' to '{2}'.", args[1], oldValue, newValue);
-            
-            // System.Console.WriteLine(xml.OuterXml);
+            C.Write("Value for '{0}' changed from '{1}' to '{2}'.",
+                    args[1], oldValue, newValue);
 
-            // System.Console.ReadLine();
+#if (DEBUG)
+            if (Debugger.IsAttached)
+                C.ReadLine();
+#endif
         }
     }
 }
